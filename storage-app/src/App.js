21d814091fa875b33f98cb8060cccc0c99476df6
aleksis <ipa21.a.velvelis@vtdt.edu.lv>
2023-11-28 09:08:07 +0200

    first commit

---
+++ storage-app/src/App.js
@@ -0,0 +1,8 @@
+import Router, { Switch, Link } from 'react-router-dom'
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline bg-red-500">
+      Hello world!
+    </h1>
+  )
+}

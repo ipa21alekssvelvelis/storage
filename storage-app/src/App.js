514569b8a551de97b5e3cb1d126548cda0ad5cda
aleksis <ipa21.a.velvelis@vtdt.edu.lv>
2023-11-29 14:11:59 +0200

    adjuted npm list

--- storage-app/src/App.js
+++ storage-app/src/App.js
@@ -1,8 +1,14 @@
-import Router, { Switch, Link } from 'react-router-dom'
-export default function App() {
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import UserActionPage from './components/UserActionPage';
+
+function App() {
   return (
-    <h1 className="text-3xl font-bold underline bg-red-500">
-      Hello world!
-    </h1>
-  )
+      <div className="min-h-screen bg-[#1d2125]">
+        <h1 className="text-3xl font-bold underline bg-red-500">
+          Hello world!
+        </h1>
+      </div>
+  );
 }
+
+export default App;

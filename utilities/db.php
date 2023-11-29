e3d3c8d605751dfb6842a01731f3b408cd5e0dcd
aleksis <ipa21.a.velvelis@vtdt.edu.lv>
2023-11-29 10:20:45 +0200

    added db.php

--- utilities/db.php
+++ utilities/db.php
@@ -1,7 +1,32 @@
 <?php
 class Database{
     private $host;
-    
-}
+    private $user;
+    private $pass;
+    private $dbname;
+    public $conn;
 
+    function __construct(){
+        $this->host = "localhost";
+        $this->user = "root";
+        $this->pass = "root";
+        $this->dbname = "shoes";
+        $this->conn = new mysqli ($this->host, $this->user, $this->pass, $this->dbname);
+    }
+
+    function insert($query){
+        if($this->conn->query($query) === TRUE){
+            return $this->conn->insert_id;
+        } else {
+            $error = $this->conn->error;
+            echo "Error: $error";
+            return false;
+        }
+    }
+    
+
+    function select($query){
+        return $this->conn->query($query);
+    }
+}
 ?>
